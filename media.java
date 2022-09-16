bb4e97f16f456c41a4e4fb37b8c3a881d99eb384
AmandaRibeiro524 <amanda.524.ribeiro@gmail.com>
2022-09-16 16:35:04 -0300

    adicionando comentários

--- media.java
+++ media.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class media {
- public static void main(String[] args) {
+
+public static void main(String[] args) {
+
+        //DEFININDO VARIÁVEIS
 		float notaP1, notaP2, notaA1, notaP3, media, notaFinal;
 		Scanner entrada = new Scanner(System.in);
 		
 		//COLETANDO AS NOTAS
 		System.out.println("Informe a nota da P1: ");
 		notaP1 = entrada.nextFloat();
+
 		System.out.println("Informe a nota da P2: ");
 		notaP2 = entrada.nextFloat();
+
 		System.out.println("Informe a nota da A1: ");
 		notaA1 = entrada.nextFloat();
 		
@@ -17,21 +22,20 @@ public class media {
 		media = (notaP1 + notaP2 *2 + notaA1)/4;
 		System.out.println("A média final é = " + media);
 		
+        //ESTRUTURANDO CONDIÇÕES
 		if (media >=6 ) {
-			System.out.println(" Aprovado! ");
-			
+			System.out.println("Aprovado! ");
 		}
 	
-			
 		else if(media <6 ){
-			System.out.println(" Reprovado! ");
+			System.out.println("Reprovado! ");
 			
-			//COLETANDO NOTA DA RECUPERAÇÃO
+		//COLETANDO NOTA DA RECUPERAÇÃO
 			Scanner entrada2 = new Scanner(System.in);
-			System.out.println(" Inserir nota da Recuperação: ");
+			System.out.println("Inserir nota da Recuperação: ");
 			notaP3 = entrada2.nextFloat();	
 			
-			//CALCULO FINAL
+		//CALCULO FINAL
 			notaFinal =(media + notaP3)/2;
  
 			if (notaP3 >=6) {
@@ -41,7 +45,7 @@ public class media {
 				System.out.println("Reprovado de vez! ");
 } 
 			}
-		
+
 	}
 }
 
